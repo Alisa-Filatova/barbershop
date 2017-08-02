39eab6617a362fbeccf32d0055ec05f41330d209
Alisa Filatova <alicefill@bk.ru>
2017-08-02 20:40:44 +0300

    исправлена карта

--- js/modal.js
+++ js/modal.js
@@ -7,29 +7,31 @@ var password = popup.querySelector("[name=password]");
 var storage = localStorage.getItem("login");
 
 link.addEventListener("click", function(event) {
-event.preventDefault();
-popup.classList.add("modal-content-show");
+	event.preventDefault();
+	popup.classList.add("modal-content-show");
+
 	if (storage) {
 		login.value = storage;
 		password.focus();
-} else {
+	} else {
 		login.focus();
-}
+	}
 });
 
 close.addEventListener("click", function(event) {
-event.preventDefault();
-popup.classList.remove("modal-content-show");
+	event.preventDefault();
+	popup.classList.remove("modal-content-show");
 });
 
 form.addEventListener("submit", function(event) {
+
 	if (!(login.value && password.value)) {
 		event.preventDefault();
 		popup.classList.remove("modal-error");
 		popup.classList.add("modal-error");
-} else {
+	} else {
 		localStorage.setItem("login", login.value);
-}
+	}
 });
 
 window.addEventListener("keydown", function(event) {
@@ -44,13 +46,11 @@ window.addEventListener("keydown", function(event) {
 var mapOpen = document.querySelector(".open-map");
 var mapPopup = document.querySelector(".modal-content-map");
 var mapClose = mapPopup.querySelector(".modal-content-close");
-var mapCanvas = document.getElementById("map-canvas");
+
 
 mapOpen.addEventListener("click", function(event) {
 	event.preventDefault();
 	mapPopup.classList.add("modal-content-show");
-	google.maps.event.trigger(mapCanvas, 'resize');
-	map.setCenter(new LatLng(59.9387942,30.3230833));
 });
 
 
@@ -61,7 +61,8 @@ mapClose.addEventListener("click", function(event) {
 
 window.addEventListener("keydown", function(event) {
 	if (event.keyCode == 27) {
-	  	if (mapPopup.classList.contains("modal-content-show")) {
+
+		if (mapPopup.classList.contains("modal-content-show")) {
 			mapPopup.classList.remove("modal-content-show");
 	  	}
 	}

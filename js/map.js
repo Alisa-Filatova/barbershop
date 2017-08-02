d1c5971e4ed0a7fdc661413eb16e1959de6867f4
Alisa Filatova <alicefill@bk.ru>
2017-08-02 18:30:24 +0300

    исправлены ссылки

--- js/map.js
+++ js/map.js
@@ -5,10 +5,10 @@ function displayMap() {
 function initialize() {
     var mapCanvas = document.getElementById('map-canvas');
     var mapOptions = {
-        scrollwheel: false, 
-        zoom: 17,        
-        center: new google.maps.LatLng(59.9403473,30.3190278)
-        }      
+            scrollwheel: false,
+            zoom: 17,
+            center: new google.maps.LatLng(59.9403473,30.3190278)
+        };
     var map = new google.maps.Map(mapCanvas, mapOptions);
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(59.9387942,30.3230833),
--- js/modal.js
+++ js/modal.js
@@ -9,11 +9,11 @@ var storage = localStorage.getItem("login");
 link.addEventListener("click", function(event) {
 event.preventDefault();
 popup.classList.add("modal-content-show");
-if (storage) {
-	login.value = storage;
-	password.focus();
+	if (storage) {
+		login.value = storage;
+		password.focus();
 } else {
-	login.focus();
+		login.focus();
 }
 });
 
@@ -23,21 +23,22 @@ popup.classList.remove("modal-content-show");
 });
 
 form.addEventListener("submit", function(event) {
-if (!(login.value && password.value)) {
-	event.preventDefault();
-	popup.classList.remove("modal-error");
-	popup.classList.add("modal-error");
+	if (!(login.value && password.value)) {
+		event.preventDefault();
+		popup.classList.remove("modal-error");
+		popup.classList.add("modal-error");
 } else {
-	localStorage.setItem("login", login.value);
+		localStorage.setItem("login", login.value);
 }
 });
 
 window.addEventListener("keydown", function(event) {
-if (event.keyCode == 27) {
-	if (popup.classList.contains("modal-content-show")) {
-		popup.classList.remove("modal-content-show");
+	if (event.keyCode == 27) {
+
+		if (popup.classList.contains("modal-content-show")) {
+			popup.classList.remove("modal-content-show");
+		}
 	}
-}
 });
 
 var mapOpen = document.querySelector(".open-map");
@@ -46,22 +47,22 @@ var mapClose = mapPopup.querySelector(".modal-content-close");
 var mapCanvas = document.getElementById("map-canvas");
 
 mapOpen.addEventListener("click", function(event) {
-event.preventDefault();
-mapPopup.classList.add("modal-content-show");
-google.maps.event.trigger(mapCanvas, 'resize');
-map.setCenter(new LatLng(59.9387942,30.3230833)); 
+	event.preventDefault();
+	mapPopup.classList.add("modal-content-show");
+	google.maps.event.trigger(mapCanvas, 'resize');
+	map.setCenter(new LatLng(59.9387942,30.3230833));
 });
 
 
 mapClose.addEventListener("click", function(event) {
-event.preventDefault();
-mapPopup.classList.remove("modal-content-show");
+	event.preventDefault();
+	mapPopup.classList.remove("modal-content-show");
 });
 
 window.addEventListener("keydown", function(event) {
-if (event.keyCode == 27) {
-  if (mapPopup.classList.contains("modal-content-show")) {
-    mapPopup.classList.remove("modal-content-show");
-  }
-}
+	if (event.keyCode == 27) {
+	  	if (mapPopup.classList.contains("modal-content-show")) {
+			mapPopup.classList.remove("modal-content-show");
+	  	}
+	}
 });
